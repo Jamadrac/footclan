6066fbe8522ebd8b027dbacf08ac7657bbf23c17
jamadra <rabbikindalo001@gmail.com>
2024-06-12 14:46:03 +0200

    aa

--- webserver/src/App.jsx
+++ webserver/src/App.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import ProtectedRoute from "./ProtectedRoute";
 import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import Home from "./screens/Home";
 import Signup from "./screens/register";
+
 function App() {
   return (
     <RecoilRoot>
       <Router>
         <Routes>
-          <ProtectedRoute path="/profile" element={<HomeScreen />} />
+          {/* <ProtectedRoute path="/profile" element={<HomeScreen />} /> */}
           <Route path="/login" element={<LoginScreen />} />
           <Route path="/register" element={<Signup />} />
           <Route path="/" element={<Home />} />
