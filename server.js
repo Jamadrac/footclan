8109a151d1d1354ccff492025e431fc96810f1fe
jamadra <rabbikindalo001@gmail.com>
2024-06-30 20:34:40 +0200

    sd

--- server.js
+++ server.js
@@ -15,15 +15,15 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "*"); // Allow all HTTP methods
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "*"); // Allow all HTTP methods
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 app.use(express.json());
 app.use(morgan("tiny"));
