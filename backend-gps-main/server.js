e6f4f17597c814abd61785e665a64fb5301f6e77
jamadra <rabbikindalo001@gmail.com>
2024-06-30 13:04:48 +0200

    Changed status to 200 and message });

--- backend-gps-main/server.js
+++ backend-gps-main/server.js
@@ -17,7 +17,8 @@ const port = 8000;
 
 // Basic GET route
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome to the API"); //
+  res.status(200).json("Welcome to the API"); // Changed status to 200 and message
+});
 
 // API routes
 app.use("/api", router);
