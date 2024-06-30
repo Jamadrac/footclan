8d446da973a0cb9294f7b77b06dc0c9f385b050d
jamadra <rabbikindalo001@gmail.com>
2024-06-30 15:49:43 +0200

    sd

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import connect from "./database/conn.js";
 import router from "./router/route.js";
+import allowCors from "./cors.js";
 
 const app = express();
 
@@ -16,7 +17,7 @@ const app = express();
 //   optionSuccessStatus: 200,
 // };
 // app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(allowCors));
 app.use(express.json());
 app.use(morgan("tiny"));
 
@@ -26,7 +27,7 @@ const port = 8000;
 
 // Basic GET route
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome to the CORS-enabled 999999 server");
+  res.status(200).json("Welcome to the CORS-enabled 998888899 server");
 });
 
 // API routes
