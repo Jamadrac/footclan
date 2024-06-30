6f3870393d599e8299bb77ceac28fc41fb51918f
jamadra <rabbikindalo001@gmail.com>
2024-06-30 15:38:06 +0200

    as

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ import router from "./router/route.js";
 const app = express();
 
 // Middleware
-const corsOptions = {
-  origin: "*", // Allow all origins
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", // Allow all methods
-  allowedHeaders:
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token", // Allow all headers
-  credentials: true, // Allow cookies and credentials
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: "*", // Allow all origins
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS", // Allow all methods
+//   allowedHeaders:
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization, X-Auth-Token", // Allow all headers
+//   credentials: true, // Allow cookies and credentials
+//   optionSuccessStatus: 200,
+// };
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
 
@@ -25,7 +26,7 @@ const port = 8000;
 
 // Basic GET route
 app.get("/", (req, res) => {
-  res.status(200).json("Welcome to the CORS-enabled fff server");
+  res.status(200).json("Welcome to the CORS-enabled 999999 server");
 });
 
 // API routes
