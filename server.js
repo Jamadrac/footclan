e257b624ef02f33127ea9497590e61668c3cab69
jamadrac <rabbikindalo001@gmail.com>
2024-07-14 11:41:21 +0200

    as

--- server.js
+++ server.js
@@ -2,20 +2,16 @@ import express from "express";
 import morgan from "morgan";
 import connect from "./database/conn.js";
 import router from "./router/route.js";
-import cors from "cors"
-// import allowCors from "./allowCors.js";
+import cors from "cors";
 
 const app = express();
-
-// Use allowCors middleware for all routes
-// app.use(allowCors((req, res, next) => {
-//   next();
-// }));
 
 // Other middleware
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(cors())
+
+// Allow all CORS requests
+app.use(cors());
 
 const port = 8000;
 
